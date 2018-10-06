b97605ee5dec6fc13104a3e11d6d3deecc49b9c8
John Azzaro <jazzaro@temple.edu>
2018-10-05 22:37:00 -0700

    improved js

--- index.js
+++ index.js
@@ -3,51 +3,47 @@
 const STORE = [
   {name: "apples", checked: false},
   {name: "oranges", checked: false},
-  {name: "milk", checked: true},
+  {name: "milk", checked: false},
   {name: "bread", checked: false}
 ];
 
+function getItemIndexFromElement(item) {
+    const itemIndexString = $(item)
+      .closest('.js-item-index-element')
+      .attr('data-item-index');
+    return parseInt(itemIndexString, 10);
+  }
+
+function addItemToShoppingList(itemName) {
+    STORE.push({name: itemName, checked: false});
+  }
 
 function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
       <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span> 
-    </li>`;
+    </li>
+    `
 }
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
   return items.join("");
 }
 
 function renderShoppingList() {
-  console.log('`renderShoppingList` ran');
   const shoppingListItemsString = generateShoppingItemsString(STORE);
   $('.js-shopping-list').html(shoppingListItemsString);
-}
-
-function addItemToShoppingList(itemName) {
-  console.log(`Adding "${itemName}" to shopping list`);
-  STORE.push({name: itemName, checked: false});
 }
 
 function handleNewItemSubmit() {
   $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    console.log('`handleNewItemSubmit` ran');
     const newItemName = $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
     addItemToShoppingList(newItemName);
     renderShoppingList();
   });
-}
-
-function getItemIndexFromElement(item) {
-  const itemIndexString = $(item)
-    .closest('.js-item-index-element')
-    .attr('data-item-index');
-  return parseInt(itemIndexString, 10);
 }
 
 function handleShoppingList() {
