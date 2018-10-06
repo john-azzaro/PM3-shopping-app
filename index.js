ec49b87f570f584fd2567c832d16a2f94b501ae1
John Azzaro <jazzaro@temple.edu>
2018-10-05 18:42:07 -0700

    initial, pseudocode, function stubs, etc.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 "use strict";
 
 
+
+
 function renderShoppingList() {
     const shoppingListItemString = generateShoppingListItemString(STORE)
     $('main').html(shoppingListItemString);
 }
 
-
-function handleShoppingList() {
-    renderShoppingList();
+function handleNewItemSubmit() {
+    $(".js-shopping-list-form").submit(function(event) {
+        event.preventDefault();
+        //
+    })
 }
 
+// callback for when the page loads, rendering shopping list, activating individual
+function handleShoppingList() {
+    renderShoppingList();
+    handleNewItemSubmit();
+}
+// when page loads, call "handleShoppingList"
 $(handleShoppingList);
