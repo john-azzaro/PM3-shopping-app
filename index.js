d6d2309e0c23a09abc45def38c290b05b4449ae3
John Azzaro <jazzaro@temple.edu>
2018-10-05 18:54:25 -0700

    additional .js code

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ function renderShoppingList() {
     $('main').html(shoppingListItemString);
 }
 
+// when the user submits a new iem, push new item onto the store and re-render list from the DOM.
 function handleNewItemSubmit() {
+    // upon the user clicking the submit button...
     $(".js-shopping-list-form").submit(function(event) {
+        // prevent defualt form submission behavior (no reset)...
         event.preventDefault();
-        //
+        // new item variable...
+        const newItemName = $('.js-shopping-list-entry').val();
+        $('.js-shopping-list-entry').val("").
+        addItemToShoppingList();
+        renderShoppingList();
     })
 }
 
-// callback for when the page loads, rendering shopping list, activating individual
+// callback for when the page loads, rendering shopping list, activating individual functions.
 function handleShoppingList() {
     renderShoppingList();
     handleNewItemSubmit();
